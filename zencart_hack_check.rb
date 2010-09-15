ffbe4844bed5a0ff76688b45b9d55ca2aab190cd
Paul Hepworth <paul@peppyheppy.com>
2010-09-15 05:41:07 +0000

    added basic arguments for execution, and added readme

--- zencart_hack_check.rb
+++ zencart_hack_check.rb
@@ -1,11 +1,11 @@
 #!/bin/env ruby
 
 require 'net/smtp'
-def send_email(from, from_alias, to, to_alias, subject, message)
+def send_email(from, to, subject, message)
 
 	msg = <<-END_OF_MESSAGE
-From: #{from_alias} <#{from}>
-To: #{to_alias} <#{to}>
+From: #{from}
+To: #{to}
 Subject: #{subject}
 	
 #{message}
@@ -16,15 +16,15 @@ END_OF_MESSAGE
   end
 end
 
-%w(domain1 domain2).each do |account|
+raise "Arguments must be:\ncpanel,acount,names from@email.com to@email.com" unless ARGV.size == 3
+
+ARGV[0].split(',').each do |account|
   output = `cd /home/#{account}/www; git status`
 
   if output.split("\n").map { |m| /[^includes\/configure]\.(php)/i.match(m) }.compact.size > 0 then
     send_email(
-      'root@host.com',
-      'System Monitory', 
-      'paul@site.com', 
-      'Webmaster', 
+      ARGV[1],
+      ARGV[2], 
       "#{account} might have been hacked?", 
       "You might want to check it out.:\n\n#{output}"
     )
