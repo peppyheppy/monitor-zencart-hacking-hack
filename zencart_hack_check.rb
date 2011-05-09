a1082edabc3456792af806cec29f489fe97462ee
Paul Hepworth <paul@peppyheppy.com>
2011-05-09 06:46:44 +0000

    added removeal of file

--- zencart_hack_check.rb
+++ zencart_hack_check.rb
@@ -1,6 +1,8 @@
 #!/bin/env ruby
-
+require 'rubygems'
+require 'fileutils'
 require 'net/smtp'
+require 'ruby-debug'
 def send_email(from, to, subject, message)
 
 	msg = <<-END_OF_MESSAGE
@@ -19,15 +21,17 @@ end
 raise "Arguments must be:\ncpanel,acount,names from@email.com to@email.com" unless ARGV.size == 3
 
 ARGV[0].split(',').each do |account|
-  output = `cd /home/#{account}/www; git status`
-
-  if output.split("\n").map { |m| /[^includes\/configure]\.(php)/i.match(m) }.compact.size > 0 then
+  root_dir = "/home/#{account}/www/"
+  output = `cd #{root_dir}; git status`
+  files = output.split("\n").map { |m| file = /.*[^includes]\.(php)/i.match(m); file && root_dir + file[0].gsub("#\t","") }.compact
+  if files.size > 0 then
     send_email(
       ARGV[1],
       ARGV[2], 
       "#{account} might have been hacked?", 
       "You might want to check it out.:\n\n#{output}"
     )
+    FileUtils.rm files
   end
 
 end
